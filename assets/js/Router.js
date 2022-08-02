cf785d82584774644eee1a856e7a84cd566195c2
Mr.black <black@08liter.com>
2022-08-02 09:11:31 +0900

    add item / rout link 추가

---
+++ assets/js/Router.js
@@ -0,0 +1,30 @@
+export const RouteLink = [
+  {
+    id: 1,
+    title: 'Home',
+    to: '/'
+  },
+  {
+    id: 2,
+    title: 'About US',
+    to: '/about'
+  },
+  {
+    id: 3,
+    title: 'Contact US',
+    to: 'contact'
+  }
+];
+
+export const RouteItems = [
+  {
+    icon: 'mdi-apps',
+    title: 'Home',
+    to: '/'
+  },
+  {
+    icon: 'mdi-chart-bubble',
+    title: 'Photos',
+    to: '/photos'
+  }
+];
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,117 +1,53 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
+    <v-app-bar app>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn v-for="(item, i) in items" :key="i" @click="$router.push(item.to)">
+          {{item.title}}
+        </v-btn>
+      </v-toolbar-items>
     </v-app-bar>
     <v-main>
       <v-container>
         <Nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-footer
       :absolute="!fixed"
       app
     >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
+      <v-row no-gutters>
+        <v-btn v-for="link in links" :key="link.id" color="white" text rounded class="my-2" :to="link.to">
+          {{ link.title }}
+        </v-btn>
+        <v-col class="py-4 text-left white--text" cols="12">
+          <div>
+            <span>전화번호: </span>
+            <span>
+              <a href="tel:02-1444-1555">02-1444-1555</a>
+            </span>
+          </div>
+          <div>
+            <span>
+              부산광역시 남구 8부두로 27
+            </span>
+          </div>
+        </v-col>
+      </v-row>
     </v-footer>
   </v-app>
 </template>
 
 <script>
+import { RouteLink, RouteItems } from "../assets/js/Router";
 export default {
   name: 'DefaultLayout',
   data () {
     return {
-      clipped: false,
-      drawer: false,
       fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
+      items: RouteItems,
+      links: RouteLink,
       miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
     }
   }
 }
