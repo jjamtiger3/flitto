ea4c6e5ed8ba5a3d8e547e1bd4e35b2754d272f0
Mr.black <black@08liter.com>
2022-08-03 10:01:28 +0900

    포토 상세  api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const fs = require("fs");
 const photosPath = __dirname + '/data/photos.json';
 const encoding = 'utf-8';
 
@@ -12,6 +13,16 @@ app.get('/api/photos', (req, res) => {
     const data = fs.readFileSync(photosPath);
     res.send(data);
 });
+app.get('/api/photo/:id', (req, res) => {
+  const fs = require('fs');
+  const data = fs.readFileSync(photosPath, encoding);
+  const id = req.params.id;
+  const json = JSON.parse(data);
+  var json_data = json.filter(_json_data => _json_data.id === id)[0];
+  res.json({
+    data: json_data
+  });
+});
 app.get('/api/photos/:index', (req, res) => {
   const fs = require('fs');
   const data = fs.readFileSync(photosPath, encoding);
@@ -21,11 +32,16 @@ app.get('/api/photos/:index', (req, res) => {
   // 1. json의 길이가 index * page보다 큰지 체크
   // 2. index * page + page보다 큰경우 그대로 실행
   // 3. index * page + page보다 작은경우 index * page만 실행
-  var json_data = json;
+  var json_data = json.map((_json_data) => {
+    const { id, download_url } = _json_data;
+    return {
+      id, download_url
+    }
+  });
   if (json.length > (index * page) + page) {
-    json_data = json.splice(index * page, page);
+    json_data = json_data.splice(index * page, page);
   } else if (json.length > index * page && json.length < (index * page) + page) { // 마지막 데이터
-    json_data = json.splice(index * page);
+    json_data = json_data.splice(index * page);
   } else {
     json_data = [];
   }
